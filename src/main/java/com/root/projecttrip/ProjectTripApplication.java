ef07fd124cab11ebb1bfa9c2e072c574d1182301
joonoo <ksop6580@naver.com>
2023-07-16 12:38:03 +0900

    Test

---
+++ src/main/java/com/root/projecttrip/ProjectTripApplication.java
@@ -0,0 +1,13 @@
+package com.root.projecttrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/root/projecttrip/ProjectTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.projecttrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
